125d11d60c4f061baad6ad50aa3f5d22fd10d43c
nizzlo <nisalsamaranayake@gmail.com>
2019-07-19 17:09:40 +0530

    1 file modified

--- src/app/hero-service.service.ts
+++ src/app/hero-service.service.ts
@@ -4,6 +4,7 @@ import { HEROES } from './mock-heroes'
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 
 @Injectable(
@@ -20,7 +21,9 @@ export class HeroServiceService {
     // TODO: send the message _after_ fetching the heroes
     //this.messageService.add('HeroService: fetched heroes');
     //return of (HEROES);
-     return this.http.get<Hero[]>(this.heroesUrl)
+     return this.http.get<Hero[]>(this.heroesUrl).pipe(
+       catchError(this.handleError<Hero[]>('getHeroes',[]))
+     )
   }
 
   getHero(id: number): Observable<Hero> {
@@ -31,6 +34,20 @@ export class HeroServiceService {
 
 private log(message: string) {
   this.messageService.add(`HeroService: ${message}`);
+}
+
+private handleError<T> (operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+ 
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+ 
+    // TODO: better job of transforming error for user consumption
+    this.log(`${operation} failed: ${error.message}`);
+ 
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
 }
 
 }
